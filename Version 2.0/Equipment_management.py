beea6c5c3138b037e3acd918b1262cd38ef4dae2
Rao <103417697+CodingAssassins@users.noreply.github.com>
2023-05-11 12:02:52 +0400

    Added Equipment Analysis ✅ + Equipment Database ✅

---
+++ Version 2.0/Equipment_management.py
@@ -0,0 +1,31 @@
+from rich import print
+from rich import text
+from rich.panel import Panel
+from rich.table import Table
+from rich.console import Console
+
+from rich.traceback import install
+install(show_locals=True)
+
+from datetime import datetime
+import numpy as np
+import pandas as pd
+import asciichartpy
+import math
+
+def equip_management():
+    csv_path = 'EquipmentDB.csv'
+    # Read the CSV file into a Pandas DataFrame
+    data = pd.read_csv(csv_path)
+    
+    # Initialize the table
+    EDB_table = Table(show_header=True, header_style="bold magenta")
+    
+    # Add columns to the table
+    for column in data.columns:
+        EDB_table.add_column(column)
+    
+    # Add rows to the table
+    for row in data.values.tolist():
+        EDB_table.add_row(*row)
+    return Panel(EDB_table, title = "Equipment Database", title_align = "left", style = "Bold White")
