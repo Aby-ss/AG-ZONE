d34aae067f94f95e254360ee591e8abf84bd839c
Rao <103417697+CodingAssassins@users.noreply.github.com>
2023-05-12 18:24:09 +0400

    Fixed broken Table printing ✅

--- Version 2.0/Equipment_management.py
+++ Version 2.0/Equipment_management.py
@@ -1,29 +1,23 @@
-from rich import print
-from rich import text
-from rich.panel import Panel
-from rich.table import Table
 from rich.console import Console
-
-from rich.traceback import install
-install(show_locals=True)
-
-from datetime import datetime
-import numpy as np
+from rich.table import Table
 import pandas as pd
-import asciichartpy
-import math
 
-def equip_management():
-    Equipment_DB = Table.grid(expand = True)
-    Equipment_DB.add_column("Order No.", justify = "center", ratio = 1)
-    Equipment_DB.add_column("Location", justify = "center")
-    Equipment_DB.add_column("Status", justify = "right")
-    Equipment_DB.add_column("Customer Name", justify = "right")
-    Equipment_DB.add_row("[b]Order No.", "[b]Location", "[b]Status", "[b]Customer Name")
-    Equipment_DB.add_row("456754678", "12th street, avenue", "[b green]In process", "Allen")
-    Equipment_DB.add_row("546769867", "Main street, City block 12", "[b red]Late", "Chris")
-    Equipment_DB.add_row("465787655", "City Centre, 22nd House", "[b yellow]Pending", "Cristiano")
-    Equipment_DB.add_row("564467658", "24th House, Birmingham", "[b yellow]Pending", "Adam")
-    Equipment_DB.add_row("687786578", "City Centre", "[b green]Done", "Leonel")
+# Read CSV file into a pandas DataFrame
+data = pd.read_csv("C:\\Users\\hadir\\Documents\\VSC - Projects\\Python\\AG-ZONE\\Version 2.0\\EquipmentDB.csv")
 
-equip_management()
+# Create a Rich table object
+table = Table(title="My CSV Data")
+
+# Add header to the table
+for col in data.columns:
+    table.add_column(col)
+
+# Add data to the table
+for row in data.values:
+    table.add_row(*row)
+
+# Create a Rich console object
+console = Console()
+
+# Print the table to the console
+console.print(table)
