4d981dee85916e0f054496245499eadad83c0a51
Rao <103417697+CodingAssassins@users.noreply.github.com>
2023-05-19 17:34:38 +0400

    Updated Pest management Table ✅ + File Function UPDATED ✅

--- Version 2.0/Pest_management.py
+++ Version 2.0/Pest_management.py
@@ -6,28 +6,18 @@ from datetime import datetime, timedelta
 console = Console()
 
 # Create a table for displaying pest and disease information
-table = Table(show_header=True, header_style="bold magenta")
-table.add_column("Pest/Disease", style="dim")
-table.add_column("Description")
-table.add_column("Treatment")
+pest_table = Table(show_header=True, header_style="bold blue")
+pest_table.add_column("Pest/Disease", style="dim")
+pest_table.add_column("Description")
+pest_table.add_column("Treatment")
 
 # Add data to the table with shorter descriptions
-table.add_row("Aphids", "Small sap-feeding insects", "Use insecticidal soap or neem oil")
-table.add_row("Powdery Mildew", "Fungal disease with white powdery coating", "Apply fungicide")
-table.add_row("Leafhoppers", "Insects causing leaf yellowing", "Use insecticidal spray")
-table.add_row("Black Spot", "Fungal disease causing black spots on leaves", "Remove infected leaves and apply fungicide")
-table.add_row("Caterpillars", "Larvae of butterflies and moths eating leaves", "Handpick or use organic insecticide")
+pest_table.add_row("Aphids", "Small sap-feeding insects", "Use insecticidal soap or neem oil")
+pest_table.add_row("Powdery Mildew", "Fungal disease with white powdery coating", "Apply fungicide")
+pest_table.add_row("Leafhoppers", "Insects causing leaf yellowing", "Use insecticidal spray")
+pest_table.add_row("Black Spot", "Fungal disease causing black spots on leaves", "Remove infected leaves and apply fungicide")
+pest_table.add_row("Caterpillars", "Larvae of butterflies and moths eating leaves", "Handpick or use organic insecticide")
 
 # Print the whole table
-console.print(table)
+console.print(pest_table)
 
-# Pest scheduler
-console.print("\n[bold cyan]Pest Scheduler:[/bold cyan]\n")
-
-current_date = datetime.now()
-end_date = current_date + timedelta(days=30)
-
-while current_date <= end_date:
-    console.print(f"\n[bold yellow]{current_date.strftime('%Y-%m-%d')}:[/bold yellow]\n")
-    console.print(table)
-    current_date += timedelta(days=7)
